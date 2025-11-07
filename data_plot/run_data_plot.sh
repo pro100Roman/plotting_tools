935b3e0a896d7dd15ea66481260db9029a6e5964
Roman Hvozd <h.roman.lviv@gmail.com>
2025-11-07 14:17:28 +0200

    remove absolute path form *.sh

--- data_plot/run_data_plot.sh
+++ data_plot/run_data_plot.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-SCRIPT_DIR="/Volumes/Macintosh HD/Users/roman/Work/Tools/Software"
+SCRIPT_DIR="./"
 echo $SCRIPT_DIR
 source "$SCRIPT_DIR/.venv/bin/activate"
 python "$SCRIPT_DIR/data_plot/data_plot.py" "$@"
